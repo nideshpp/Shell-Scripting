f3ad77b83df26b9d373ff3f306c2e75ae87af522
root <root@ubuntu.myguest.virtualbox.org>
2025-10-22 23:08:31 +0530

    new push for git

---
+++ Git/auto_pull.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+<<comment
+Script to automatically pulls the latest code from git repository
+Author: Nidesh
+comment
+
+
+cd /home/ubuntu/Shell_scripts/
+
+#command
+
+git pull origin main
